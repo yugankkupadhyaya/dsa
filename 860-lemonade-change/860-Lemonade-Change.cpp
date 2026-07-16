7b8c1daae711b4ec228236fecaae9b6c76871c8f
yugankkupadhyaya <yugankkupadhyaya@gmail.com>
2026-07-17 00:50:58 +0530

    [860] [Time Beats: 100.00%] [Memory Beats: 45.77%] - LeetPush

---
+++ 860-lemonade-change/860-Lemonade-Change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+
+            if (bill == 5) {
+                five++;
+            }
+            else if (bill == 10) {
+
+                if (five == 0)
+                    return false;
+
+                five--;
+                ten++;
+            }
+            else { // bill == 20
+
+                // Prefer giving one $10 and one $5
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                // Otherwise give three $5 bills
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
