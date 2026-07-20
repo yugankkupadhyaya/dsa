7ab6796324b39eb91e02fb5dc5ff644140b3db6f
yugankkupadhyaya <yugankkupadhyaya@gmail.com>
2026-07-20 20:48:16 +0530

    [1260] [Time Beats: 100.00%] [Memory Beats: 39.39%] - LeetPush

---
+++ DCP-07-26/1260-Shift-2D-Grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = m * n;
+
+        k %= total;
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                ans[newIndex / n][newIndex % n] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
