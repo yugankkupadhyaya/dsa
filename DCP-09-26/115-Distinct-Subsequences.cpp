9a308e9b988b1f898cfb93aec7471220f8104448
yugankkupadhyaya <yugankkupadhyaya@gmail.com>
2026-09-06 20:57:19 +0530

    [115] [Time Beats: 73.87%] [Memory Beats: 52.57%] - LeetPush

---
+++ DCP-09-26/115-Distinct-Subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                dp[i][j] = dp[i-1][j];
+
+                if (s[i-1] == t[j-1]) {
+                    if (dp[i-1][j-1] <= INT_MAX - dp[i][j]) {
+                        dp[i][j] += dp[i-1][j-1];
+                    } else {
+                        dp[i][j] = INT_MAX;
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
